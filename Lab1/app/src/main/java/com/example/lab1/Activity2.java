b6ae92e6fd68081d593971cdc3e73adf2d3d693c
ilya <ilya.m.shapiro@gmail.com>
2020-11-06 12:21:35 +0300

    modified lab1, added styles from lab2

--- Lab1/app/src/main/java/com/example/lab1/Activity2.java
+++ Lab1/app/src/main/java/com/example/lab1/Activity2.java
@@ -2,12 +2,14 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -16,24 +18,26 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         TextView tv = (TextView) findViewById(R.id.text1);
-        if (getIntent().getStringExtra("number")!=null){
-            tv.setText(getIntent().getStringExtra("number"));
-        }
         EditText etext = findViewById(R.id.editText);
+        Integer input = getIntent().getIntExtra("number", 0);
+        tv.setText(input.toString());
 
         Button button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (!etext.getText().toString().equals("")&&etext.getText().toString().matches("-?\\d+(\\.\\d+)?")) {
-                    Intent intent = new Intent(Activity2.this, MainActivity.class);
-                    intent.putExtra("number",
-                            Integer.parseInt(tv.getText().toString())
-                                    + Integer.parseInt(etext.getText().toString()) + "");
-                    startActivity(intent);
+                if (!etext.getText().toString().equals("")){
+                    Intent returnIntent = new Intent();
+                    returnIntent.putExtra("result", Integer.parseInt(tv.getText().toString())
+                            + Integer.parseInt(etext.getText().toString()));
+                    setResult(Activity.RESULT_OK, returnIntent);
+                    finish();
                 }
+                else
+                    Toast.makeText(Activity2.this, R.string.error_message, Toast.LENGTH_SHORT).show();
+
             }
         });
-
     }
 }
--- Lab1/app/src/main/java/com/example/lab1/MainActivity.java
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,24 +18,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         TextView tv = (TextView) findViewById(R.id.text1);
-        if (getIntent().getStringExtra("number")!=null){
-            tv.setText(getIntent().getStringExtra("number"));
-        }
-
         EditText etext = findViewById(R.id.editText);
-
-        Button button = (Button) findViewById(R.id.button1);
+        Button button = findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (!etext.getText().toString().equals("")&&etext.getText().toString().matches("-?\\d+(\\.\\d+)?")) {
+                if (!etext.getText().toString().equals("")){
                     Intent intent = new Intent(MainActivity.this, Activity2.class);
-                    intent.putExtra("number",
-                            Integer.parseInt(tv.getText().toString())
-                                    + Integer.parseInt(etext.getText().toString()) + "");
-                    startActivity(intent);
+                    intent.putExtra("number", Integer.parseInt(tv.getText().toString())
+                            + Integer.parseInt(etext.getText().toString()));
+                    startActivityForResult(intent, 1);
                 }
+                else
+                    Toast.makeText(MainActivity.this, R.string.error_message, Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                Integer result = data.getIntExtra("result", 0);
+                TextView tv = (TextView) findViewById(R.id.text1);
+                tv.setText(result.toString());
+                EditText etext = findViewById(R.id.editText);
+                etext.setText("");
+            }
+        }
     }
 }
